e8ff7ebb8f3ddc41b2f96f48540567bd7e764fd1
MezianeHadjadj <mezianeh3@gmail.com>
2016-02-18 23:40:57 +0100

    create a new client to chat with socket.io socke.on function :

--- server.js
+++ server.js
@@ -8,3 +8,24 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 
 var server = app.listen(8000);
 console.log('Server listening on port 8000');
+
+
+// Socket.IO part
+var io = require('socket.io')(server);
+
+var sendChats = function (socket) {
+	fs.readFile('chat_data.json', 'utf8', function(err, chats) {
+		chats = JSON.parse(chats);
+		socket.emit('chats', chats);
+	});
+};
+
+io.on('connection', function (socket) {
+  console.log('New client connected!');
+  
+  socket.on('fetchChats', function () {
+		sendChats(socket);
+	});
+
+	
+});
